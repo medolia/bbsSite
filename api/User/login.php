9e7990541cad1a789f00b45a42cad7ea695a78e3
melodia <458972801@qq.com>
2020-06-05 20:37:50 +0800

    initialize

---
+++ api/User/login.php
@@ -0,0 +1,41 @@
+<?php
+// include database and object files
+// include_once '[filename]': includes and evaluates the specified file(only once)
+include_once '../config/database.php';
+include_once '../objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$user = new User($db);
+// set ID property of user to be edited
+// isset([variable]) return TRUE when variable exists and is NOT NULL
+// (exp1) ? (exp2) : (exp3) exp1 jugs, exp2 - True var, exp3 - False var
+$user->username = isset($_GET['username']) ? $_GET['username'] : die();
+$user->password = base64_encode(isset($_GET['password']) ? $_GET['password'] : die());
+// read the details of user to be edited
+$stmt = $user->login();
+if($stmt->rowCount() > 0){
+    // get retrieved row
+    // PDO::FETCH_ASSOC tells PDO to return the result as an associative array
+    // PDOStatement::fetch: fetches the next row from a result set
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // create array
+    $user_arr=array(
+        "status" => true,
+        "message" => "Successfully Login!",
+        "id" => $row['id'],
+        "username" => $row['username']
+    );
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "Invalid Username or Password!",
+    );
+}
+// make it json format
+print_r(json_encode($user_arr));
+?>
---
+++ api/objects/user.php
@@ -0,0 +1,86 @@
+<?php
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $id;
+    public $username;
+    public $password;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // signup user
+    function signup(){
+    
+        if($this->isAlreadyExist()){
+            return false;
+        }
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    username=:username, password=:password, created=:created";
+    
+        // prepare query, return a statement object
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->username=htmlspecialchars(strip_tags($this->username));
+        $this->password=htmlspecialchars(strip_tags($this->password));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+    
+        // bind values
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(":created", $this->created);
+    
+        // execute 'prepared' query, return TRUE on success
+        if($stmt->execute()){
+            // lastInsertId() return the line num of the inserted row
+            $this->id = $this->conn->lastInsertId();
+            return true;
+        }
+    
+        return false;
+        
+    }
+    // login user
+    function login(){
+        // select all query
+        $query = "SELECT
+                    `id`, `username`, `password`, `created`
+                FROM
+                    " . $this->table_name . " 
+                WHERE
+                    username='".$this->username."' AND password='".$this->password."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        return $stmt;
+    }
+    function isAlreadyExist(){
+        $query = "SELECT *
+            FROM
+                " . $this->table_name . " 
+            WHERE
+                username='".$this->username."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        // rowCount() returns the number of rows affected by the statement
+        if($stmt->rowCount() > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
